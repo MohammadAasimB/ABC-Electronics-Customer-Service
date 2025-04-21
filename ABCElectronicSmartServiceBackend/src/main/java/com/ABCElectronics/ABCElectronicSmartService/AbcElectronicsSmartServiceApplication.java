c9b76e66d833e88b59405e1658db56483e21f136
Mohammad Aasim <mohammadaasim2010>
2025-04-21 17:29:06 +0530

    adding the project

---
+++ ABCElectronicSmartServiceBackend/src/main/java/com/ABCElectronics/ABCElectronicSmartService/AbcElectronicsSmartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ABCElectronics.ABCElectronicSmartService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcElectronicsSmartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcElectronicsSmartServiceApplication.class, args);
+	}
+
+}
---
+++ ABCElectronicSmartServiceBackend/src/test/java/com/ABCElectronics/ABCElectronicSmartService/AbcElectronicsSmartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ABCElectronics.ABCElectronicSmartService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcElectronicsSmartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
